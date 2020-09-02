bc59cf539fb62f60d030c59f7a3e7bb68d5fdc14
Fredrik Jansson <fjansson@abo.fi>
2020-09-02 13:14:42 +0200

    setup script: pin numba version to 0.48.0, since 0.51.1 does not work

--- setup_env_cartesius.sh
+++ setup_env_cartesius.sh
@@ -9,7 +9,12 @@ module load Tkinter/3.6.6-foss-2018b-Python-3.6.6
 # setup, first time:
 #python3 -m venv --system-site-packages testbed_env
 #source testbed_env/bin/activate
-#pip install matplotlib==2.2.5 numba xarray "dask[complete]"
+#pip install matplotlib==2.2.5 numba==0.48.0 xarray "dask[complete]"
+
 source testbed_env/bin/activate
+
+
+# Notes
+# numba 0.51.1 does not work, llvm version incompatibility.
 
 
